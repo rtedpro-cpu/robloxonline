263a5516885a243f0d6bdead14494e54e7f86216
rtedpro-cpu <admihouse.tip@gmail.com>
2024-08-30 12:29:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 echo "Roblox In Web Browser Server Installer"
-echo "Recommended OS: Ubuntu 20.04/22.04"
-echo "Debian can only launch 2 instances."
 echo "Installing Now in /root/rblxweb/"
 sudo apt update
 sudo apt install docker docker.io -y
