d7ea9f83639181a94657a4f648579c62ae776890
rtedpro-cpu <admihouse.tip@gmail.com>
2024-08-31 16:17:15 +0200

    Finish Server Runner Installer

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
 echo "Roblox In Web Browser Server Installer"
-echo "Installing Now in /root/rblxweb/"
 sudo apt update
-sudo apt install docker docker.io -y
+sudo apt install adb unzip wget docker docker.io -y
+sudo systemctl start docker
 sudo mkdir /root/rblxweb
-sudo 
+sudo wget -O /root/rblxweb/latest.zip https://github.com/rtedpro-cpu/robloxonline/releases/download/2640a/roblox.zip
+sudo mkdir /root/rblxweb/dataimages
+sudo mkdir /root/rblxweb/gameusers
+sudo cd /root/rblxweb/
+sudo unzip latest.zip
+sudo rm latest.zip
+echo "Installation finished! (Warring client website isnt included so run next installer"
