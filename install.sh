02197fc1dc5f18cc8544fbdcbac7e93bc9900cb8
rtedpro-cpu <admihouse.tip@gmail.com>
2024-09-02 18:47:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install adb unzip wget docker docker.io -y
 sudo systemctl start docker
 sudo mkdir /root/rblxweb
-sudo wget -O /root/rblxweb/startinstance.sh https://github.com/rtedpro-cpu/robloxonline/blob/main/startinstance.sh
+sudo wget -O /root/rblxweb/startinstance.sh "https://github.com/rtedpro-cpu/robloxonline/raw/main/startinstance.sh"
 sudo wget -O /root/rblxweb/latest.zip https://github.com/rtedpro-cpu/robloxonline/releases/download/2640a/roblox.zip
 sudo mkdir /root/rblxweb/dataimages
 sudo mkdir /root/rblxweb/gameusers
