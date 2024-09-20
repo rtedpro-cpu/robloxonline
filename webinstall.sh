3fe81dc1486cacae65ead958d7c8369d12d6ae1a
rtedpro-cpu <admihouse.tip@gmail.com>
2024-09-20 13:44:55 +0200

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+if [ ! -d "/root/rblxweb" ]; then
+    echo "Please run the main installer then web client installer"
+    exit 1
+fi
+echo Starting To Install
+echo Starting To Cache
 
