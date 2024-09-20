43798daf301246f50dc6c187507c9e34e3f7784f
rtedpro-cpu <admihouse.tip@gmail.com>
2024-09-20 13:59:36 +0200

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -6,7 +6,7 @@ fi
 echo "Roblox In Browser Server 2024-2024™ - WebClient"
 echo "Starting To Cache"
 echo "Verifying"
-if [ ! -d "/usr/bin/docker" ]; then
+if [ ! -d "/usr/bin/docker*" ]; then
     echo "Is Docker Installed?"
     exit 1
 fi
