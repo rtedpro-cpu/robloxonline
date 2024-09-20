284a0cb2c0d58c66c5273371b4bb6b0c13cace73
rtedpro-cpu <admihouse.tip@gmail.com>
2024-09-20 13:56:03 +0200

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -3,6 +3,10 @@ if [ ! -d "/root/rblxweb" ]; then
     echo "Please run the main installer then web client installer"
     exit 1
 fi
-echo Starting To Install
-echo Starting To Cache
-
+echo "Roblox In Browser Server 2024-2024™ - WebClient"
+echo "Starting To Cache"
+echo "Verifying"
+if [ ! -d "/usr/bin/docker" ]; then
+    echo "Is Docker Installed?"
+    exit 1
+fi
