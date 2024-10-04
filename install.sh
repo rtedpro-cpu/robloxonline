18ddefa142fb2b17a5462fb418bf9ab653dc7232
rtedpro-cpu <admihouse.tip@gmail.com>
2024-10-04 17:45:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ sudo apt install adb unzip wget qemu-kvm docker docker.io -y
 sudo systemctl start docker
 sudo mkdir /root/rblxweb
 sudo wget -O /root/rblxweb/startinstance.sh "https://github.com/rtedpro-cpu/robloxonline/raw/main/startinstance.sh"
-sudo wget -O /root/rblxweb/latest.zip https://github.com/rtedpro-cpu/robloxonline/releases/download/2640a/roblox.zip
+sudo wget -O /root/rblxweb/latest.zip https://github.com/rtedpro-cpu/robloxonline/releases/download/2642a/2642a.zip
 sudo mkdir /root/rblxweb/dataimages
 sudo mkdir /root/rblxweb/gameusers
 cd /root/rblxweb/
 sudo unzip latest.zip
 sudo rm latest.zip
-echo "Installation finished! (Warring client website isnt included so run next installer"
+echo "Installation finished!
